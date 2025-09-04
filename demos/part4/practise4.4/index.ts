990c9b4fa143f8d6b9db076586eacdc776908bff
enforcer-a06 <enforcer.a06@gmail.com>
2025-09-04 19:02:34 +0800

    feat: wip部分4的学习

--- demos/part4/practise4.4/index.ts
+++ demos/part4/practise4.4/index.ts
@@ -1,6 +1,20 @@
+import type { Scene as TScene, OrthographicCamera as TOrthographicCamera, WebGLRenderer as TWebGLRenderer, Mesh as TMesh } from 'three';
+import { Scene, OrthographicCamera, WebGLRenderer } from 'three';
+
 class MPRViewer {
-  constructor() {
-    console.log('MPRViewer');
+  private container: HTMLElement;
+  private scene: TScene;
+  private camera: TOrthographicCamera;
+  private renderer: TWebGLRenderer;
+  private plane?: TMesh;
+  constructor(element: HTMLElement) {
+    this.container = element;
+    this.scene = new Scene();
+    this.camera = new OrthographicCamera(this.container.clientWidth / -2, this.container.clientWidth / 2, this.container.clientHeight / 2, this.container.clientHeight / -2, -5000, 5000);
+    this.camera.position.set(0, 0, 10);
+    this.renderer = new WebGLRenderer();
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    this.container.appendChild(this.renderer.domElement);
   }
 }
 
